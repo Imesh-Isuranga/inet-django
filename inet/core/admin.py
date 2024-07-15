a1d06b1eb839ef94f13cf15ab217350d0f032e9d
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-07-15 16:52:04 +0530

    Updated like option

--- inet/core/admin.py
+++ inet/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from core.models import Profile,Post
+from core.models import LikePost, Profile,Post
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
+admin.site.register(LikePost)
 
