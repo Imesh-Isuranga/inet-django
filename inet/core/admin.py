55d0f7b499ad788e1b3a48eceb0ef799c001458a
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-07-15 18:29:53 +0530

    add following and followers option

--- inet/core/admin.py
+++ inet/core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from core.models import LikePost, Profile,Post
+from core.models import FollowersCount, LikePost, Profile,Post
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(LikePost)
+admin.site.register(FollowersCount)
 
