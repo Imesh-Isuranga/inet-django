af219d7254d797f5746b0639d6fae77456622556
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-07-15 14:06:04 +0530

    add uploading post option

--- inet/core/admin.py
+++ inet/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import Profile
+from core.models import Profile,Post
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Post)
 
--- inet/core/views.py
+++ inet/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
-from core.models import Profile
+from .models import Post, Profile
 from django.contrib.auth.decorators import login_required
 
 
@@ -10,7 +10,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request,'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request,'index.html',{'user_profile':user_profile})
 
 @login_required(login_url='signin')
 def settings(request):
@@ -95,3 +97,17 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image,caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
