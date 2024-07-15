f5549adfb5f03789235c0340dbaada15f38446a4
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-07-15 11:20:49 +0530

    Added settings,sign in and signup.

--- inet/core/views.py
+++ inet/core/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
 @login_required(login_url='signin')
 def index(request):
     return render(request,'index.html')
